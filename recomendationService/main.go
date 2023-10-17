4d26fde3b5f2a81d967cb2ba8aed2407290b5232
ansenya <ars.anoshin@gmail.com>
2023-10-17 23:59:56 +0300

    все еще бессмысленный комит

--- recomendationService/main.go
+++ recomendationService/main.go
@@ -2,98 +2,150 @@ package main
 
 import (
 	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"time"
 )
 
-// Video структура представляет видео на платформе
-type Video struct {
-	ID          int
-	Title       string
-	Description string
-	Tags        []string
+type UserModel struct {
+	ID       int64 `gorm:"primary_key"`
+	Username string
+	//Password string
+	Name    string
+	Surname string
+	Sex     string
+	//Pfp            ImageModel `gorm:"ForeignKey:ImageID"`
+	Age int
+	//Role string
+	//Country CountryModel
+	Channels []ChannelModel `gorm:"ForeignKey:UserID"`
+	//Subscriptions  []ChannelModel  `gorm:"many2many:subscriptions"`
+	//Comments       []CommentModel  `gorm:"ForeignKey:UserID"`
+	//LikedVideos    []VideoModel    `gorm:"many2many:videos_liked"`
+	//DislikedVideos []VideoModel    `gorm:"many2many:videos_disliked"`
+	//Playlists      []PlaylistModel `gorm:"ForeignKey:OwnerID"`
+	//Created time.Time
+	//Updated time.Time
+	//Deleted bool `gorm:"column:deleted"`
+	//Locked  bool
 }
 
-// UserProfile структура представляет профиль пользователя
-type UserProfile struct {
-	Gender   string
-	Age      int
-	Liked    []int
-	Disliked []int
-	Tags     []string
+type ImageModel struct {
+	ID      int64 `gorm:"primary_key"`
+	Path    string
+	Type    ImageType `gorm:"ForeignKey:TypeID"`
+	Created time.Time
+	Updated time.Time
+	Deleted bool
 }
 
-// RecommendVideos функция для рекомендации видео на основе контентной фильтрации
-func RecommendVideos(userProfile UserProfile, videos []Video, numRecommendations int) []Video {
-	// Создайте карту для хранения сходства между видео и профилем пользователя
-	videoSimilarity := make(map[int]float64)
-
-	// Рассчитайте сходство между видео и профилем пользователя
-	for _, video := range videos {
-		similarity := calculateContentSimilarity(userProfile, video)
-		videoSimilarity[video.ID] = similarity
-	}
-
-	// Отсортируйте видео по убыванию сходства
-	recommendedVideos := make([]Video, 0)
-	for i := 0; i < numRecommendations; i++ {
-		maxSimilarity := -1.0
-		recommendedVideoID := -1
-		for videoID, similarity := range videoSimilarity {
-			if similarity > maxSimilarity {
-				maxSimilarity = similarity
-				recommendedVideoID = videoID
-			}
-		}
-		if recommendedVideoID == -1 {
-			break
-		}
-		recommendedVideos = append(recommendedVideos, videos[recommendedVideoID-1])
-		delete(videoSimilarity, recommendedVideoID)
-	}
-
-	return recommendedVideos
+type CountryModel struct {
+	ID      int64 `gorm:"primary_key"`
+	Country string
+	Users   []UserModel `gorm:"ForeignKey:CountryID"`
 }
 
-// Функция для расчета сходства между профилем пользователя и видео
-func calculateContentSimilarity(userProfile UserProfile, video Video) float64 {
-	// Пример: просто сравниваем количество общих тегов
-	commonTags := 0
-	for _, userTag := range userProfile.Tags {
-		for _, videoTag := range video.Tags {
-			if userTag == videoTag {
-				commonTags++
-			}
-		}
-	}
-	// Вы можете использовать более сложные методы для вычисления сходства
-	// на основе контента, такие как TF-IDF или Word2Vec.
+type ChannelModel struct {
+	ID int64 `gorm:"primary_key"`
+	//Name string
+	//VideosAmount int
+	// FollowersAmount можно вычислять на лету
+	//Pfp       ImageModel   `gorm:"ForeignKey:PfpID"`
+	//Videos    []VideoModel `gorm:"ForeignKey:ChannelID"`
+	//Posts     []PostModel  `gorm:"ForeignKey:ChannelID"`
+	User UserModel `gorm:"ForeignKey:UserID"`
+	//Followers []UserModel  `gorm:"many2many:subscriptions"`
+	//Deleted   bool
+	//Created   time.Time
+	//Updated   time.Time
+}
 
-	// В этом примере просто возвращаем количество общих тегов.
-	return float64(commonTags)
+type CommentModel struct {
+	ID      int64 `gorm:"primary_key"`
+	Text    string
+	Deleted bool
+	Video   VideoModel `gorm:"ForeignKey:VideoID"`
+	User    UserModel  `gorm:"ForeignKey:UserID"`
+	Created time.Time
+	Updated time.Time
+}
+
+type VideoModel struct {
+	ID             int64 `gorm:"primary_key"`
+	Name           string
+	Description    string
+	Duration       int64
+	Views          int64
+	Thumbnail      ImageModel     `gorm:"ForeignKey:ThumbnailID"`
+	Comments       []CommentModel `gorm:"ForeignKey:VideoID"`
+	Channel        ChannelModel   `gorm:"ForeignKey:ChannelID"`
+	Category       CategoryModel  `gorm:"ForeignKey:CategoryID"`
+	Path           string
+	Tags           []TagModel      `gorm:"many2many:videos_tags"`
+	Playlists      []PlaylistModel `gorm:"many2many:videos_playlists"`
+	LikedByUser    []UserModel     `gorm:"many2many:liked_videos"`
+	DislikedByUser []UserModel     `gorm:"many2many:disliked_videos"`
+	VidUUID        string
+	Processed      bool
+	Explicit       bool
+	Deleted        bool
+	Stream         bool
+	Created        time.Time
+	Updated        time.Time
+}
+
+type PlaylistModel struct {
+	ID     int64        `gorm:"primary_key"`
+	Owner  UserModel    `gorm:"ForeignKey:OwnerID"`
+	Videos []VideoModel `gorm:"many2many:videos_playlists"`
+}
+
+type TagModel struct {
+	ID  int64  `gorm:"primary_key"`
+	Tag string `gorm:"column:tag"`
+}
+
+type CategoryModel struct {
+	ID       int64 `gorm:"primary_key"`
+	Category string
+	Items    []VideoModel `gorm:"ForeignKey:CategoryID"`
+}
+
+type ImageType struct {
+	ID   int64 `gorm:"primary_key"`
+	Type string
+}
+
+type PostModel struct {
+	ID      int64 `gorm:"primary_key"`
+	Title   string
+	Content string
+	Images  []ImageModel `gorm:"ForeignKey:PostID"`
+	Polls   PollModel    `gorm:"ForeignKey:PostID"`
+}
+
+type PollModel struct {
+	ID       int64 `gorm:"primary_key"`
+	Question string
+	Elements []PollElement `gorm:"ForeignKey:PollID"`
+}
+
+type PollElement struct {
+	ID     int64 `gorm:"primary_key"`
+	Answer string
+	User   UserModel `gorm:"ForeignKey:UserID"`
 }
 
 func main() {
-	// Пример данных: создайте профиль пользователя и видео
-	userProfile := UserProfile{
-		Gender:   "Male",
-		Age:      25,
-		Liked:    []int{1, 2, 3},
-		Disliked: []int{4, 5},
-		Tags:     []string{"funny", "music", "travel", "sports"},
+	dsn := "newuser:@tcp(localhost:3306)/tube"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database")
 	}
 
-	videos := []Video{
-		{1, "Funny Cats", "Cute cats doing funny things", []string{"cats", "funny", "animals"}},
-		{2, "Music Video", "Latest music video release", []string{"music", "entertainment"}},
-		{3, "Travel Vlog", "Explore the world's most beautiful places", []string{"travel", "adventure", "vacation"}},
-		{4, "Cooking Tutorial", "Learn to cook delicious recipes", []string{"cooking", "food", "recipes"}},
-		{5, "Sports Highlights", "Exciting sports moments", []string{"sports", "highlights", "athletics"}},
-	}
+	var res []UserModel
+	db.Raw("SELECT * FROM users ").Scan(&res)
 
-	// Рекомендация видео
-	recommendedVideos := RecommendVideos(userProfile, videos, 3)
-
-	fmt.Println("Рекомендуемые видео:")
-	for _, video := range recommendedVideos {
-		fmt.Printf("ID: %d, Заголовок: %s\n", video.ID, video.Title)
-	}
+	fmt.Println(res)
 }
