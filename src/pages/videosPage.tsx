202659bf44b6d49ff81d0a652ac02f1dbd9a1afc
ansenya <ars.anoshin@gmail.com>
2024-02-28 16:09:06 +0300

    document title spot main fixef

--- src/pages/videosPage.tsx
+++ src/pages/videosPage.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import VideoScroll from "../components/videosScroll";
 
+
 const VideosPage = () => {
+  
+  useEffect(() => {
+    document.title = "Spot"
+  });
+
   return <><VideoScroll/></>
 };
 
